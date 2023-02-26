8c5da6e41a68b4aea7ca52043a023db63ed3280c
Gavin Klibowitz <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2023-02-26 00:49:18 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from github import Github
+import sys
 import os
 
+# get the personal access token from the command-line arguments
+token = sys.argv[1]
+
 # authenticate to GitHub API using personal access token
-g = Github(os.getenv('GITHUB_TOKEN'))
+g = Github(token)
 
 # get repository
-repo = g.get_repo('owner/repo')
+repo = g.get_repo('CY83R-3X71NC710N/Weather-Dashboard-Updater')
 
 # get all issues in the repository
 issues = repo.get_issues(state='open')
@@ -13,3 +17,6 @@ issues = repo.get_issues(state='open')
 # iterate over the issues and close each one
 for issue in issues:
     issue.edit(state='closed')
+
+print("Warning: Clearing command-line history...")
+os.system("history -c")
